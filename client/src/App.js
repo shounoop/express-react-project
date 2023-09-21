e354e025332cefeb6ed2361007836efce6b648cd
Shounoop <hmtuan9a@gmail.com>
2023-09-21 21:30:49 +0700

    fetching the api

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,18 @@
+import React, { useEffect, useState } from 'react';
+
 function App() {
+	const [backendData, setBackendData] = useState();
+
+	useEffect(() => {
+		fetch('/api')
+			.then((response) => response.json())
+			.then((data) => {
+				setBackendData(data);
+			});
+	}, []);
+
+	console.log('backendData', backendData);
+
 	return <div></div>;
 }
 
